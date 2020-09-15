4e39ff86b7603aa585e9f3cf9426739f56a9ad2d
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-09-14 21:37:14 -0500

    Initial Commit, Work on spacing.

---
+++ Unit_2/Stringliteral.java
@@ -0,0 +1,9 @@
+package Unit_2;
+
+public class Stringliteral {
+    public static void main(String args[]){
+        System.out.println("\"HOIZONTAL ASYMPTOTES\nIN LOVE\"\n\n");
+        System.out.print("Five. Seven. "+"Then five.\nThe universal answer.\nMath, nature as one.\n\n");
+        System.out.print("--Billimarie\n  March "+24+", " + Integer.toString(20) + Integer.toString(11));
+    }
+}
