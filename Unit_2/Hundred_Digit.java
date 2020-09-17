a649f5a1ec2f94ce552e32a5a94311ad89fa78c8
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-09-17 10:03:42 -0500

    Completed Hundred Project

---
+++ Unit_2/Hundred_Digit.java
@@ -0,0 +1,9 @@
+package Unit_2;
+
+public class Hundred_Digit {
+    public static void main(String args[]){
+        final int NUMBER = 15387;
+        int digit = (NUMBER/100)%10;
+        System.out.println(digit);
+    }
+}
