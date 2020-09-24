caf33b259c0105a5255241985354b3315a0cd7c8
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-09-23 20:14:01 -0500

    Fixed 4 digit center thing in telephone.java

--- Unit_2/Math_Projects/Telephone.java
+++ Unit_2/Math_Projects/Telephone.java
@@ -7,7 +7,7 @@ public class Telephone {
         System.out.println("The randomly generated telephone number is: "+
        (int)(Math.random() * 9) + (int)(Math.random() * 9) + (int)(Math.random() * 9) +
        "-" +
-       String.format("%04d",(int)(Math.random() * 742)) +
+       String.format("%03d",(int)(Math.random() * 742)) +
        "-" +
        String.format("%04d",(int)(Math.random() * 10000))
         );
