cfd9002cd90d75220f21bff49bdac61a124dc2c6
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-10-03 15:09:58 -0500

    Revert

--- Unit_2/Hundred_Digit.java
+++ Unit_2/Hundred_Digit.java
@@ -2,12 +2,6 @@ package Unit_2;
 
 public class Hundred_Digit {
     public static void main(String args[]){
-        //This is a comment.
-        //Check if it pushed
-        /*
-        Multi Line
-        Multi Line.
-        */
         final int NUMBER = 15387;
         int digit = (NUMBER/100)%10;
         System.out.println("The digit in the hundreds place is: " + digit);
