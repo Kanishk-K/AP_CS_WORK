5b89d76a04214df9f060c54b20590619c5d1a375
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-10-09 18:21:17 -0500

    Table format for Multiplication Table

--- Unit_3/ProgrammingProjectsPart1/MultiplicationTable.java
+++ Unit_3/ProgrammingProjectsPart1/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     public static void main(String args[]){
         for (int i = 1;i<=12;i++){
             for (int j = 1;j<=12;j++){
-                System.out.print(""+(i*j)+" ");
+                System.out.print(""+(i*j)+"\t");
             }
             System.out.print("\n");
         }
