f0cb1e68bb3e01c54b163efa0d9a9e26a503ccad
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-10-14 22:09:36 -0500

    SlotMachine finished

---
+++ Unit_3/ProgrammingProjectsPart2/SlotMachine.java
@@ -0,0 +1,32 @@
+package Unit_3.ProgrammingProjectsPart2;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+public class SlotMachine {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String another = "y";
+        int random;
+        Set<Integer> Slots = new HashSet<Integer>();
+        while (another.equalsIgnoreCase("y")){
+            for(int i = 0;i<3;i++){
+                random = (int)(Math.random()*10);
+                System.out.print(Integer.toString(random)+" ");
+                Slots.add(random);
+            }
+            if(Slots.size() == 3){
+                System.out.println("\nUnfortunately you got none to match.");
+            }
+            else if(Slots.size() == 2){
+                System.out.println("\nCongrats, you matched two!");
+            }
+            else{
+                System.out.println("\nJACKPOT!");
+            }
+            Slots.clear();
+            System.out.println("Type \"y\" if you want to play again.");
+            another=scan.nextLine();
+        }
+        scan.close();
+    }
+}
