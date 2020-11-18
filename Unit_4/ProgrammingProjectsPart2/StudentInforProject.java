dd6b78e51bdad5b8c34f8384b58893adf78e6e47
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-11-17 20:06:19 -0600

    Completed SlopeIntercept Project

--- Unit_4/ProgrammingProjectsPart2/StudentInforProject.java
+++ Unit_4/ProgrammingProjectsPart2/StudentInforProject.java
@@ -1,9 +1,8 @@
 package Unit_4.ProgrammingProjectsPart2;
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class StudentInforProject {
-    public class Address{
+    public static class Address{
         private String streetAddress, city, state;
         private int zipCode;
         //-----------------------------------------------------------------
@@ -28,6 +27,7 @@ public class StudentInforProject {
     public static class Student{
         private String firstName, lastName;
         private Address homeAddress, schoolAddress;
+        private int[] testScores = new int[3];
         //-----------------------------------------------------------------
         // Sets up this Student object with the specified initial values.
         //-----------------------------------------------------------------
@@ -45,11 +45,37 @@ public class StudentInforProject {
             String result;
             result = firstName + " " + lastName + "\n";
             result += "Home Address:\n" + homeAddress + "\n";
-            result += "School Address:\n" + schoolAddress;
+            result += "School Address:\n" + schoolAddress + "\n";
+            result +=  getTestScore(1)+ " "+getTestScore(2)+ " "+getTestScore(3)+ " Average: "+ average();
             return result;
+        }
+        public void setTestScore(int test_number,int score){
+            testScores[test_number-1] = score;
+        }
+        public String getTestScore(int test_number){
+            return "Score for test#"+test_number+": " +testScores[test_number-1];
+        }
+        // Get the average and reuturn it as a double.
+        public double average(){
+            return Arrays.stream(testScores).sum()/testScores.length;
+        }
+        // Return the student's name as a String.
+        public String getName(){
+            return firstName + " " + lastName;
         }
     }
     public static void main(String args[]){
-        
+        Address school = new Address ("800 Lancaster Ave.", "Villanova","PA", 19085);
+        Address jHome = new Address ("21 Jump Street", "Lynchburg","VA", 24551);
+        Student john = new Student ("John", "Gomez", jHome, school);
+        Address mHome = new Address ("123 Main Street", "Euclid", "OH",44132);
+        Student marsha = new Student ("Marsha", "Jones", mHome, school);
+        marsha.setTestScore(1, 30);
+        marsha.setTestScore(2, 32);
+        marsha.setTestScore(3, 40);
+        System.out.println("Testing out your getTestScore method: " + john.getTestScore(2));
+        System.out.println (john);
+        System.out.println ();
+        System.out.println (marsha);
     }
 }
